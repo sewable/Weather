1b5328fc6c740540591e220d677de04b68aebf17
homework <sewerynwojcik95@gmail.com>
2021-05-29 11:25:16 +0200

    tests 2

--- src/test/java/com/weather/LocationService.java
+++ src/test/java/com/weather/LocationService.java
@@ -1,2 +1,11 @@
-package com.weather;public class LocationService {
+package com.weather;
+
+import org.junit.jupiter.api.Test;
+
+public class LocationService {
+
+    @Test
+    public Location createNewLocation(String cityName, Double length, Double width, String region, String country) {
+
+    }
 }
